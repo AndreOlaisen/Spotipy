d08e97908de0590e009ca5b022c1c25dde9fad63
AndreOlaisen <56491067+AndreOlaisen@users.noreply.github.com>
2020-05-25 09:23:20 +0200

    Update duplicate_detection.py

--- duplicate_detection.py
+++ duplicate_detection.py
@@ -1,7 +1,6 @@
 import spotipy
 import spotipy.util as util
 import pandas as pd
-from get_playlist import get_playlist
 
 
 def print_song(song):
@@ -14,6 +13,93 @@ def print_song(song):
 def print_song_uri(song_uri):
     song = sp.track(song_uri)
     print_song(song)
+    
+    
+def get_playlist(token, playlist_uri):
+    sp = spotipy.Spotify(auth=token)
+    result = sp.playlist(playlist_uri, fields=None, market=None, additional_types=('track',))
+    print()
+    print(result['name'])
+    print('  total tracks', result['tracks']['total'])
+    results = sp.playlist(result['id'],
+                          fields="tracks,next")
+    tracks = results['tracks']
+    show_tracks(tracks)
+    list_of_artist_names = []
+    list_of_artist_uri = []
+    list_of_song_names = []
+    list_of_song_uri = []
+    list_of_durations_ms = []
+    list_of_explicit = []
+    list_of_albums = []
+    list_of_album_uri = []
+    list_of_album_release_date = []
+    list_of_popularity = []
+
+    while tracks['next']:
+        for i, item in enumerate(tracks['items']):
+            track = item['track']
+            this_artists_name = track["artists"][0]["name"]
+            list_of_artist_names.append(this_artists_name)
+            this_artists_uri = track["artists"][0]["uri"]
+            list_of_artist_uri.append(this_artists_uri)
+            list_of_songs = track["name"]
+            list_of_song_names.append(list_of_songs)
+            song_uri = track["uri"]
+            list_of_song_uri.append(song_uri)
+            list_of_duration = track["duration_ms"]
+            list_of_durations_ms.append(list_of_duration)
+            song_explicit = track["explicit"]
+            list_of_explicit.append(song_explicit)
+            this_album_uri = track["album"]["uri"]
+            list_of_album_uri.append(this_album_uri)
+            this_album = track["album"]["name"]
+            list_of_albums.append(this_album)
+            release_date = track["album"]["release_date"]
+            list_of_album_release_date.append(release_date)
+            song_popularity = track["popularity"]
+            list_of_popularity.append(song_popularity)
+        tracks = sp.next(tracks)
+    else:
+        for i, item in enumerate(tracks['items']):
+            track = item['track']
+            this_artists_name = track["artists"][0]["name"]
+            list_of_artist_names.append(this_artists_name)
+            this_artists_uri = track["artists"][0]["uri"]
+            list_of_artist_uri.append(this_artists_uri)
+            list_of_songs = track["name"]
+            list_of_song_names.append(list_of_songs)
+            song_uri = track["uri"]
+            list_of_song_uri.append(song_uri)
+            list_of_duration = track["duration_ms"]
+            list_of_durations_ms.append(list_of_duration)
+            song_explicit = track["explicit"]
+            list_of_explicit.append(song_explicit)
+            this_album_uri = track["album"]["uri"]
+            list_of_album_uri.append(this_album_uri)
+            this_album = track["album"]["name"]
+            list_of_albums.append(this_album)
+            release_date = track["album"]["release_date"]
+            list_of_album_release_date.append(release_date)
+            song_popularity = track["popularity"]
+            list_of_popularity.append(song_popularity)
+
+
+    all_songs = pd.DataFrame(
+        {'artist': list_of_artist_names,
+         'artist_uri': list_of_artist_uri,
+         'song': list_of_song_names,
+         'song_uri': list_of_song_uri,
+         'duration_ms': list_of_durations_ms,
+         'explicit': list_of_explicit,
+         'album': list_of_albums,
+         'album_uri': list_of_album_uri,
+         'release_date': list_of_album_release_date,
+         'popularity': list_of_popularity
+
+         })
+
+    return all_songs
 
 
 def search_for_earlier_occurrences(song_uri):
@@ -29,7 +115,7 @@ def search_for_earlier_occurrences(song_uri):
     search_result = sp.search(track_name, limit=10, offset=0, type='track', market="NO")
 
     # TODO: Avoid large duration changes
-    # TODO: album > copilation
+    # TODO: album > compilation
     for result in search_result["tracks"]["items"]:
 
         res_release_date = result["album"]["release_date"]
